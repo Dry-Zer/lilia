be869b57aa86abc2b316d2e0c574a45412a66f96
Dry-Zer <56358156+Dry-Zer@users.noreply.github.com>
2019-11-06 19:12:28 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const prefix = "!"
+
+client.on('message', message => {
+
+  if (message.content.startsWith(prefix + 'mpall')){
+    let messageArray = message.content.split(" "); let cmd = messageArray[0]; let args = messageArray.slice(1);
+
+                if(message.channel.type === "dm") return;
+                if(message.deletable) message.delete();
+
+                let MpMessage = args.slice(0).join(" ");
+
+                message.guild.members.forEach(member => {
+                    member.send(MpMessage)
+    })
+ }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!nitro') {
+    msg.reply('MP un fondateur si tu as les invitations requises !');
+  }
+});
+
+client.login(process.env.token)
